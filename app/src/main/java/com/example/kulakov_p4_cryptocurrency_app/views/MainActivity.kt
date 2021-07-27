73827164debd011a3d0f6e528bf7f51166001ded
Alexander <a.kulakov.vironit@vironit.ru>
2021-07-27 13:22:19 +0300

    feat: create splash screen

---
+++ app/src/main/java/com/example/kulakov_p4_cryptocurrency_app/views/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.kulakov_p4_cryptocurrency_app.views
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }, 2000)
+    }
+}
