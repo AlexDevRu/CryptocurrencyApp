d2f73287dea04fad9c4afa9f70955d2de2cd449b
Alexander <a.kulakov.vironit@vironit.ru>
2021-07-27 12:50:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kulakov_p4_cryptocurrency_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kulakov_p4_cryptocurrency_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
