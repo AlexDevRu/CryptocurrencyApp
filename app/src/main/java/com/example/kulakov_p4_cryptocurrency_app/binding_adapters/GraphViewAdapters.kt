c8d2644dc92077f59210376306caaf037a4848d4
Alexander <a.kulakov.vironit@vironit.ru>
2021-08-06 15:44:42 +0300

    feat: create custom line chart

--- app/src/main/java/com/example/kulakov_p4_cryptocurrency_app/binding_adapters/GraphViewAdapters.kt
+++ app/src/main/java/com/example/kulakov_p4_cryptocurrency_app/binding_adapters/GraphViewAdapters.kt
@@ -1,144 +1,18 @@
 package com.example.kulakov_p4_cryptocurrency_app.binding_adapters
 
-import android.graphics.Color
 import androidx.databinding.BindingAdapter
-import com.example.kulakov_p4_cryptocurrency_app.R
-import com.example.kulakov_p4_cryptocurrency_app.views.custom.CustomMarkerView
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.AxisBase
-import com.github.mikephil.charting.components.Legend
-import com.github.mikephil.charting.components.LegendEntry
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.formatter.ValueFormatter
-
-@BindingAdapter("noDataMessage")
-fun setNoDataMessage(lineChart: LineChart, message: String) {
-    lineChart.setNoDataText(message)
-    lineChart.invalidate()
-}
+import com.example.kulakov_p4_cryptocurrency_app.views.custom.CustomLineChart
 
 
 @BindingAdapter(value = ["data", "labels"], requireAll = true)
-fun bindGraphViewData(lineChart: LineChart, data: List<Double>, labels: Array<String>) {
-    val entries = ArrayList<Entry>()
-    for (i in data.indices) {
-        entries.add(Entry(i.toFloat(), data[i].toFloat()))
-    }
-    val dataSet = LineDataSet(entries, null)
-    dataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
-    dataSet.color = Color.MAGENTA
-    dataSet.lineWidth = 3f
-    dataSet.circleRadius = 4f
-    dataSet.setCircleColor(Color.BLUE)
-    val lineData = LineData(dataSet)
-    lineData.setDrawValues(false)
-    lineChart.data = lineData
-    lineChart.xAxis.setDrawGridLines(false)
-    lineChart.xAxis.textSize = 14f
-    lineChart.extraBottomOffset = 4f
-    lineChart.extraLeftOffset = 3f
-    lineChart.axisLeft.setDrawGridLines(false)
-    lineChart.axisRight.setDrawGridLines(false)
-    lineChart.description.isEnabled = false
-    lineChart.legend.isEnabled = false
-
-    val formatter = object : ValueFormatter() {
-        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
-            return labels[value.toInt()]
-        }
-    }
-    lineChart.xAxis.valueFormatter = formatter
-
-    lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
-
-    lineChart.axisLeft.isEnabled = false
-
-    lineChart.invalidate()
-
-    val mv = CustomMarkerView(lineChart.context, R.layout.layout_tooltip)
-    lineChart.marker = mv
+fun bindGraphViewData(lineChart: CustomLineChart, values: List<Double>, labels: Array<String>) {
+    lineChart.clearAndSetSingleDataSet(values)
+    lineChart.setXAxisValues(labels)
 }
 
-@BindingAdapter(value = ["dataList", "labels", "legendLabels"], requireAll = false)
-fun bindGraphViewDataList(lineChart: LineChart, dataList: List<List<Double>?>, labels: Array<String>, legendLabels: List<String?>? = null) {
-
-    val colors = arrayOf(R.color.purple_700, R.color.teal_700)
-
-    if(dataList.isNullOrEmpty())
-        return
-
-    val dataSets = mutableListOf<LineDataSet>()
-
-    var counter = 0
-
-    for(data in dataList) {
-        if(data == null) continue
-        val entries = mutableListOf<Entry>()
-        for (i in data.indices) {
-            val entry = Entry(i.toFloat(), data[i].toFloat())
-            entries.add(entry)
-        }
-        val dataSet = LineDataSet(entries, null)
-        dataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
-        dataSet.color = lineChart.context.resources.getColor(colors[counter++ % colors.size])
-        dataSet.lineWidth = 3f
-        dataSet.circleRadius = 4f
-        dataSet.setCircleColor(Color.BLUE)
-        dataSets.add(dataSet)
-    }
-
-    val lineData = LineData()
-    for(dataSet in dataSets)
-        lineData.addDataSet(dataSet)
-
-    lineData.setDrawValues(false)
-
-    lineChart.data = lineData
-    lineChart.xAxis.setDrawGridLines(false)
-    lineChart.xAxis.textSize = 14f
-    lineChart.extraBottomOffset = 4f
-    lineChart.extraLeftOffset = 5f
-    lineChart.axisLeft.setDrawGridLines(false)
-    lineChart.axisRight.setDrawGridLines(false)
-    lineChart.description.isEnabled = false
-    lineChart.legend.textSize = 16f
-
-    val legendEntries = mutableListOf<LegendEntry>()
-
-    if(legendLabels != null) {
-        for(i in dataSets.indices) {
-            val legendEntry = LegendEntry()
-            legendEntry.label = legendLabels[i]
-            legendEntry.form = Legend.LegendForm.SQUARE
-            legendEntry.formSize = 16f
-            legendEntry.formColor = lineChart.context.resources.getColor(colors[i])
-            legendEntries.add(legendEntry)
-        }
-
-        lineChart.legend.yOffset = 15f
-
-        lineChart.legend.setCustom(legendEntries)
-    } else {
-        lineChart.legend.isEnabled = false
-    }
-
-    val formatter = object : ValueFormatter() {
-        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
-            return labels[value.toInt()]
-        }
-    }
-
-    lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
-
-    lineChart.xAxis.valueFormatter = formatter
-
-    lineChart.axisLeft.isEnabled = false
-
-    lineChart.invalidate()
-
-    val mv = CustomMarkerView(lineChart.context, R.layout.layout_tooltip)
-    lineChart.marker = mv
+@BindingAdapter(value = ["dataList", "labels", "legendLabels"], requireAll = true)
+fun bindGraphViewDataList(lineChart: CustomLineChart, valuesList: List<List<Double>?>, labels: Array<String>, legendLabels: List<String?>) {
+    lineChart.setXAxisValues(labels)
+    lineChart.setDataSetList(valuesList)
+    lineChart.addLegendLabels(legendLabels)
 }
---
+++ app/src/main/java/com/example/kulakov_p4_cryptocurrency_app/views/custom/CustomLineChart.kt
@@ -0,0 +1,159 @@
+package com.example.kulakov_p4_cryptocurrency_app.views.custom
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.util.AttributeSet
+import android.widget.CheckBox
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import androidx.core.view.children
+import com.example.kulakov_p4_cryptocurrency_app.R
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
+
+
+class CustomLineChart @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): FrameLayout(context, attrs, defStyleAttr) {
+
+    private var legendContainer: LinearLayout
+    private var lineChart: LineChart
+
+    companion object {
+        val DEFAULT_COLORS = arrayOf(R.color.purple_700, R.color.teal_700)
+    }
+
+    private val checkBoxMap = mutableMapOf<CheckBox, LineDataSet>()
+
+    private fun setDefaultConfig() {
+        lineChart.apply {
+            xAxis.setDrawGridLines(false)
+            xAxis.textSize = 14f
+            extraBottomOffset = 4f
+            extraLeftOffset = 5f
+            axisLeft.setDrawGridLines(false)
+            axisRight.setDrawGridLines(false)
+            description.isEnabled = false
+            legend.isEnabled = false
+            xAxis.position = XAxis.XAxisPosition.BOTTOM
+            axisLeft.isEnabled = false
+            marker = CustomMarkerView(context, R.layout.layout_tooltip)
+            setNoDataText(context.getString(R.string.no_data))
+            data = LineData()
+        }
+    }
+
+    init {
+        val view = inflate(context, R.layout.layout_linear_graph, this)
+        legendContainer = view.findViewById(R.id.legendContainer)
+        lineChart = view.findViewById(R.id.graph)
+        setDefaultConfig()
+    }
+
+    fun setXAxisValues(labels: Array<String>) {
+        val formatter = object : ValueFormatter() {
+            override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+                return labels[value.toInt()]
+            }
+        }
+
+        lineChart.xAxis.valueFormatter = formatter
+    }
+
+    fun setDataSetList(valuesList: List<List<Double>?>) {
+        if(valuesList.isNullOrEmpty())
+            return
+
+        lineChart.lineData.clearValues()
+        legendContainer.removeAllViews()
+        checkBoxMap.clear()
+
+        for((i, values) in valuesList.withIndex()) {
+            val color = DEFAULT_COLORS[i % DEFAULT_COLORS.size]
+            val dataSet = setDataSet(values, color)
+            if(dataSet != null) {
+                lineChart.lineData.addDataSet(dataSet)
+                val checkBox = addCheckBox(color)
+                checkBoxMap.put(checkBox, dataSet)
+            }
+        }
+
+        lineChart.lineData.setDrawValues(false)
+
+        lineChart.invalidate()
+    }
+
+    private fun addCheckBox(color: Int = DEFAULT_COLORS[0]): CheckBox {
+        val checkBox = CheckBox(context)
+        val colorValue = context.resources.getColor(color)
+        checkBox.buttonTintList = ColorStateList.valueOf(colorValue)
+        val layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.marginEnd = 10
+        checkBox.layoutParams = layoutParams
+        checkBox.isChecked = true
+        checkBox.setOnCheckedChangeListener { checkbox, b ->
+            onCheckedChangeCheckBox(checkBox, b)
+        }
+        legendContainer.addView(checkBox)
+        return checkBox
+    }
+
+    fun addLegendLabels(legendLabels: List<String?>) {
+        var i = 0
+        legendContainer.children.forEach {
+            if(legendLabels[i] != null)
+                (it as CheckBox).text = legendLabels[i]
+            ++i
+        }
+    }
+
+    private fun onCheckedChangeCheckBox(checkBox: CheckBox, checked: Boolean) {
+        if(checked)
+            lineChart.lineData.addDataSet(checkBoxMap[checkBox])
+        else
+            lineChart.lineData.removeDataSet(checkBoxMap[checkBox])
+        lineChart.invalidate()
+    }
+
+    fun clearAndSetSingleDataSet(values: List<Double>?) {
+        legendContainer.removeAllViews()
+        val dataSet = setDataSet(values)
+        lineChart.lineData.apply {
+            clearValues()
+            addDataSet(dataSet)
+            setDrawValues(false)
+        }
+        checkBoxMap.clear()
+        legendContainer.removeAllViews()
+        lineChart.invalidate()
+    }
+
+    private fun setDataSet(values: List<Double>?, color: Int = DEFAULT_COLORS[0]): LineDataSet? {
+
+        if(values.isNullOrEmpty())
+            return null
+
+        val entries = ArrayList<Entry>()
+        for (i in values.indices) {
+            entries.add(Entry(i.toFloat(), values[i].toFloat()))
+        }
+        val dataSet = LineDataSet(entries, null)
+        dataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+        val colorValue = context.resources.getColor(color)
+        dataSet.color = colorValue
+        dataSet.lineWidth = 3f
+        dataSet.circleRadius = 4f
+        dataSet.setCircleColor(Color.BLUE)
+        return dataSet
+    }
+}
